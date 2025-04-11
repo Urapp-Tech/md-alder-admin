0ab844835cd99517d1a32849042fc49b5c0008e1
Rafayasad <rafay.asad@urapptech.com>
2025-04-11 18:01:48 +0500

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/admin',
+  base: '/',
   plugins: [react()],
 });
